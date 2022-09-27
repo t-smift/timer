7c14c799324cae5a8534772d253780777d30efc9
Taylor Smith <taylor.l.smith07@gmail.com>
2022-09-27 13:57:40 -0700

    linted timer1

--- timer1.js
+++ timer1.js
@@ -1,11 +1,11 @@
 const args = process.argv.slice(2);
 let delay = 1000;
 for (const arg of args) {
-  if (arg > 0 || arg === Number ) {
+  if (arg > 0 || arg === Number) {
     setTimeout(() => {
       process.stdout.write('\u0007');
-    }, delay * arg)
+    }, delay * arg);
   } else {
     continue;
   }
-};
+}
